cfe05c8cb5e0811748c9c4a343948b1529c41043
kof99athena <kof99athena@gmail.com>
2023-06-08 16:37:07 +0900

    ResetPW 화면 구현중

--- components/inputComponent.tsx
+++ components/inputComponent.tsx
@@ -1,41 +1,42 @@
-import React from "react";
+import React from 'react'
 import {View, TextInput, StyleSheet} from 'react-native'
 
-//props type [ TextInput컴포넌트의 각 속성들을 전달받기 위한 타입 ]
-type Props = {
-    placeholder? : string | undefined,
-    secureTextEntry? : boolean | undefined,
-    onChangeText? : (text:string)=> void | undefined
+// props type [ TextInput컴포넌트의 각 속성들을 전달받기 위한 타입 ]
+type Props= {
+    placeholder: string | undefined,
+    secureTextEntry?: boolean | undefined,
+    onChangeText?: (text:string)=>void | undefined
 }
 
 export default function InputComponent(props:Props):JSX.Element{
-    return(
+    return (
         <View style={style.container}>
-            <TextInput 
-            placeholder={props.placeholder} //컴포넌트를 사용하는 곳에서 힌트에 대한 property를 전달 받아야함.
-            secureTextEntry={props.secureTextEntry} //컴포넌트를 사용하는 곳에서 secure 기능에 대한 property를 전달 받아야함.
-            onChangeText={props.onChangeText}
-            placeholderTextColor='#c3c2c8'
-            style={style.input}></TextInput>
+            <TextInput
+                placeholder={props.placeholder}          //컴포넌트를 사용하는 곳에서 힌트에 대한 property를 전달받아야 함
+                secureTextEntry={props.secureTextEntry}  //컴포넌트를 사용하는 곳에서 secure 기능에 대한 property를 전달받아야 함
+                onChangeText={props.onChangeText}        //컴포넌트를 사용하는 곳에서 글씨변경 이벤트 콜백함수에 대한 property를 전달받아야 함
+                placeholderTextColor='#c3c2c8'               
+
+                style={style.input}></TextInput>
         </View>
     )
 }
-const style=StyleSheet.create({
+
+const style= StyleSheet.create({
     container:{
         width:'100%',
-        height:40,
+        height: 40,
         paddingLeft:16,
         paddingRight:16,
         borderWidth:1,
-        borderBottomColor:'#D3D3D3',
-        borderRadius:4,
-        backgroundColor:'white',
+        borderColor:'#D3D3D3',
+        borderRadius: 4,
+        backgroundColor: '#FAFAFA',
         marginTop:8,
-        marginBottom:8
+        marginBottom:8,
     },
     input:{
-        flex : 1, //TextInput의 높이를 container 높이 40에 맞게
-        color : '#292929',
-
-    }
+        flex:1,   //TextInput의 높이를 container높이 40에 맞게
+        color: '#292929',
+    },
 })
--- screen_login/ResetPW.tsx
+++ screen_login/ResetPW.tsx
@@ -1,21 +1,114 @@
-import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Button, Image, Alert } from 'react-native'
+
+//공통사용 컴포넌트 import
+import InputComponent from '../components/inputComponent'
+import TabComponent from '../components/TabComponent'
 
 import { StackScreenProps } from '@react-navigation/stack'
 import { LoginNavScreenList } from '../types'
-type ResetPasswordProps= StackScreenProps<LoginNavScreenList,'ResetPassword'>
+type ResetPWProps= StackScreenProps<LoginNavScreenList,'ResetPW'>
 
-export default function ResetPassword(props:ResetPasswordProps):JSX.Element{ //props객체{navigation, route}
+export default function ResetPW(props:ResetPWProps):JSX.Element{ //props객체{navigation, route}
 
     // 1. 우선 테스트 목적의 화면
+    // return (
+    //     <View style={style.root}>
+    //         <Text>RESET PASSWORD</Text>
+    //     </View>
+    // )
+
+    // 탭에 따른 화면 구성을 위한 state 변수들
+    const [tabs, setTabs]= useState<string[]>(['이메일','전화번호'])
+    const [tabIndex, setTabIndex]= useState<number>(0)
+
+    // 탭 선택에 따른 안내메세지
+    const message= [
+        '이메일을 입력하면 임시 비밀번호를 보내드립니다.',
+        '전화번호를 입력하면 임시 비밀번호를 보내드립니다.',
+    ]
+
+
+    // 2. 비밀번호 재설정 화면
     return (
         <View style={style.root}>
-            <Text>RESET PASSWORD</Text>
+            {/* 1. 콘텐츠 영역 */}
+            <View style={style.content}>
+                {/* 1.1 좌물쇠 이미지 표시 영역 */}
+                <View style={style.lockImageContainer}>
+                    <Image source={require('../Images/lock.jpg')}></Image>
+                </View>
+
+                {/* 1.2 타이틀 안내문구 표시 */}
+                <Text style={style.title}>로그인에 문제가 있나요?</Text>
+
+                {/* 1.3 이메일 또는 전화번호 선택에 대한 안내문구 */}
+                <Text style={style.message}> {message[tabIndex]} </Text>
+
+                {/* 1.4 탭 만들기 */}
+                <View style={style.tabContainer}>
+                    {
+                        tabs.map((value, index)=> {
+                            return <TabComponent label={tabs[index]} selected={index==tabIndex} onPress={()=>setTabIndex(index)} key={index}></TabComponent>
+                        })
+                    }
+                </View>
+
+                {/* 1.5 정보 입력박스 */}
+                <InputComponent placeholder={tabs[tabIndex]}></InputComponent>
+
+                {/* 1.6 전송 버튼 */}
+                <View style={{width:'100%', margin:16}}>
+                    <Button title='전송' onPress={()=>Alert.alert('임시비밀번호가 발송되었습니다.','로그인 후 정보수정을 통해 안전한 비밀번호로 변경해 주시기 바랍니다.')}></Button>
+                </View>
+
+            </View>
+
+            {/* 2. footer 영역 */}
+            <View style={style.footer}>
+                <Text style={style.goBack} onPress={()=>props.navigation.goBack()}>로그인화면으로 돌아가기</Text>
+            </View>
+
         </View>
-    )
+    ) 
 }
 
 //style sheet object
 const style= StyleSheet.create({
-    root:{ flex:1, backgroundColor:'FEFFFF' },
+    root:{ flex:1, backgroundColor:'#FEFFFF' },
+    content:{
+        flex: 1,
+        width:'100%',
+        alignItems:'center',
+        padding:32,
+    },
+    footer:{
+        borderTopWidth:1,
+        borderTopColor:'#D3D3D3',
+        padding:8,
+    },
+    goBack:{
+        color:'#3796EF',
+        textAlign:'center',
+    },
+    lockImageContainer:{
+        padding:24,
+        borderWidth:2,
+        borderColor:'#292929',
+        borderRadius: 100,
+        margin:16,
+    },
+    title:{
+        fontSize:16,
+        marginBottom:16,
+    },
+    message:{
+        textAlign:'center',
+        marginBottom:16,
+        color:'#292929',
+    },
+    tabContainer:{
+        flexDirection:'row',
+        marginBottom:16,
+    }
 })
